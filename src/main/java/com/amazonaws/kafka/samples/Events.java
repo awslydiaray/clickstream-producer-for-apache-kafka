dc7fc8ded3f85523faf6efc9feb34aca885b65e0
awslydiaray <123984242+awslydiaray@users.noreply.github.com>
2023-03-25 16:40:17 +0000

    Change sample data options

--- src/main/java/com/amazonaws/kafka/samples/Events.java
+++ src/main/java/com/amazonaws/kafka/samples/Events.java
@@ -14,12 +14,17 @@ import java.util.concurrent.atomic.AtomicLong;
 
 class Events {
     static final private String[] deviceType = {"mobile","computer", "tablet"};
-    static final private String[] productCatalogOptions = {"home_page", "product_detail"};
+//     static final private String[] productCatalogOptions = {"home_page", "product_detail"};
+    static final private String[] productCatalogOptions = {"home_page", "room_detail"};
     static final private String[] productTypeOptions = {"cell phones", "laptops", "ear phones", "soundbars", "cd players", "AirPods", "video games", "cameras"};
-    static final private String[] productDetailOptions = {"product_catalog", "add_to_cart"};
-    static final private String[] addToCartOptions = {"product_catalog", "remove_from_cart", "order"};
-    static final private String[] orderOptions = {"order_checkout", "remove_from_cart", "product_catalog"};
-    static final private String[] removeFromCartOptions = {"", "product_detail", "product_catalog"};
+//     static final private String[] productDetailOptions = {"product_catalog", "add_to_cart"};
+    static final private String[] productDetailOptions = {"room_option", "reserve_room"};
+//     static final private String[] addToCartOptions = {"product_catalog", "remove_from_cart", "order"};
+    static final private String[] addToCartOptions = {"room_option", "unreserve_room", "confirm_booking"};
+//     static final private String[] orderOptions = {"order_checkout", "remove_from_cart", "product_catalog"};
+    static final private String[] orderOptions = {"complete_booking", "unreserve_room", "room_option"};
+//     static final private String[] removeFromCartOptions = {"", "product_detail", "product_catalog"};
+    static final private String[] removeFromCartOptions = {"home_page", "room_detail", "room_option"};
     private Random rand = new Random();
     private final static AtomicLong totalEvents = new AtomicLong(0L);
     private AtomicInteger errorCount = new AtomicInteger(0);
@@ -116,24 +121,24 @@ class Events {
 
         switch (previousEventType) {
             case "home_page":
-                eventType = "product_catalog";
+                eventType = "room_option";
                 break;
-            case "product_catalog":
+            case "room_option":
                 eventType = productCatalogOptions[rand.nextInt(productCatalogOptions.length)];
                 break;
-            case "product_detail":
+            case "room_detail":
                 eventType = productDetailOptions[rand.nextInt(productDetailOptions.length)];
                 break;
-            case "add_to_cart":
+            case "reserve_room":
                 eventType = addToCartOptions[rand.nextInt(addToCartOptions.length)];
                 break;
-            case "order":
+            case "confirm_booking":
                 eventType = orderOptions[rand.nextInt(orderOptions.length)];
                 break;
-            case "order_checkout":
+            case "complete_booking":
                 eventType = "";
                 break;
-            case "remove_from_cart":
+            case "unreserve_room":
                 eventType = removeFromCartOptions[rand.nextInt(removeFromCartOptions.length)];
                 break;
         }
@@ -147,22 +152,22 @@ class Events {
             case "home_page":
                 productType = "N/A";
                 break;
-            case "product_catalog":
+            case "room_option":
                 productType = "N/A";
                 break;
-            case "product_detail":
+            case "room_detail":
                 productType = productTypeOptions[rand.nextInt(productTypeOptions.length)];
                 break;
-            case "add_to_cart":
+            case "reserve_room":
                 productType = previousProductType;
                 break;
-            case "remove_from_cart":
+            case "unreserve_room":
                 productType = previousProductType;
                 break;
-            case "order":
+            case "confirm_booking":
                 productType = previousProductType;
                 break;
-            case "order_checkout":
+            case "complete_booking":
                 productType = previousProductType;
                 break;
         }
